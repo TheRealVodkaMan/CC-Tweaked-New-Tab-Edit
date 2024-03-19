2e97ea45a6810ee286a8d3a47672dd6ad49882b0
Xerkcis <89606281+TheRealVodkaMan@users.noreply.github.com>
2024-03-19 05:48:36 +0200

    Delete custom/programs directory

--- custom/programs/custom/update.lua
+++
@@ -1,6 +0,0 @@
-os.loadAPI("custom/apis/custom_term.lua")
-
-shell.setPath(shell.path()..":/custom/programs")
-
-local comp = require("cc.shell.completion")
-shell.setCompletionFunction("custom/programs/tab_edit.lua", comp.build(comp.file))
--- custom/programs/startup.lua
+++
@@ -1,9 +0,0 @@
-shell.run("custom/update.lua")
-
-term.setTextColor(colors.yellow)
-term.setBackgroundColor(colors.blue)
-
-print("\nPrograms path has been updated!\n")
-
-term.setTextColor(colors.white)
-term.setBackgroundColor(colors.black)
